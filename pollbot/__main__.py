e06bb90f38860ca37f3cddb9c59f303fbd4dce75
Kaamkiya <kaamkiya@noreply.codeberg.org>
2024-05-28 18:26:49 -0400

    feat: Update polls.json with used polls

--- pollbot/__main__.py
+++ pollbot/__main__.py
@@ -23,7 +23,13 @@ while True:
     if poll["id"] not in used_polls:
         break
 
+# 24 hours of 60 minutes with 60 seconds each, across 3 days
 p = m.make_poll(poll["options"], 24 * 60 * 60 * 3)
 
 m.status_post(poll["question"], poll=p)
+
+polls["used"].append(poll["id"])
+
+with open("polls.json", "w", encoding="utf-8") as f:
+    f.write(json.dumps(polls, indent=2))
 
